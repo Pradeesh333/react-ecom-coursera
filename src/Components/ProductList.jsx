4c67a20fb81dc2b1272dd269c429682fc87f040b
Pradeesh333 <pradeesh.3333@gmail.com>
2024-07-23 09:56:21 +0530

    added files

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import './ProductList.css'; 
-
+import React from "react";
+import "./ProductList.css";
+//hhhh
 const ProductList = () => {
-
   const products = [
-    { id: 1, name: 'Product A', price: 60 },
-    { id: 2, name: 'Product B', price: 75 },
-    { id: 3, name: 'Product C', price: 30 },
+    { id: 1, name: "Product A", price: 60 },
+    { id: 2, name: "Product B", price: 75 },
+    { id: 3, name: "Product C", price: 30 },
   ];
 
   return (
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
-      <ul className="product-list-items">
-     
-      </ul>
+      <ul className="product-list-items"></ul>
     </div>
   );
 };
